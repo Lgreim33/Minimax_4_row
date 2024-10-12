38a2f38c9ac65ad76207a15aed0f5733bcd28fdc
Liam <lgreim33@gmail.com>
2024-10-12 11:57:53 -0500

    finishing implementation for the player 1 and two moves, the only thing left should be minimax

--- minmax_game.py
+++ minmax_game.py
@@ -16,17 +16,21 @@ def generate_sucsessors(board):
     
 
 
-#recursive minimax
+#recursive minimax, returns the best move to make for the player
 def minimax(state,is_maxing,depth,alpha,beta):
     
-    
+    #base case
+    if(depth == 0):
+        return 
     
     #we are maximizing the heuristic
     if is_maxing:
+        print("Player 1 is Maxing")
         return
         
     #minimizing the heuristic
     elif not is_maxing:
+        print("Player 2 is Mining")
         return
     return
 
@@ -35,8 +39,13 @@ def player_1_move(board):
     start_time = time.time()
     
     #two ply call for minimax
+    move = minimax(board,True,2,alpha=np.float("-inf"),beta=("inf"))
     
     end_time = time.time() - start_time
+    
+    board.move(move,1)
+    print(f"Minimax took: {end_time} seconds")
+    print(f"Player 1 Places X at {move}")
     
     return
 
@@ -47,8 +56,13 @@ def player_2_move(board):
     start_time = time.time()
     
     #four ply call for minimax
+    move = minimax(board,False,4,alpha=np.float("-inf"),beta=("inf"))
     
     end_time = time.time() - start_time
+    
+    board.move(move,0)
+    print(f"Minimax took: {end_time} seconds")
+    print(f"Player 2 Places O at {move}")
     return 
 
 
@@ -64,18 +78,18 @@ def start():
     #we will start displaying the actual turn variable after the manual intial moves
     turn = 3
     
-    player_1_move = (2,3)
-    player_2_move = (2,2)
+    player_1_init = (2,3)
+    player_2_init = (2,2)
     
     #initial moves for both players (-1,-1 for indexing)
     print("Turn 1")
-    print("Player 1 Moves: ", player_1_move)
-    board_world.move(player_1_move,1)
+    print("Player 1 Moves: ", player_1_init)
+    board_world.move(player_1_init,1)
     print(board_world.board)
     
     print("Turn 2")
-    print("Player 2 Moves: ", player_2_move)
-    board_world.move(player_2_move,0)
+    print("Player 2 Moves: ", player_2_init)
+    board_world.move(player_2_init,0)
     print(board_world.board)
     
     #while the board isnt full and nobody has won, play the game
